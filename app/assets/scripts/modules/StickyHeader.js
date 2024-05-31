395708764d52934354c18d2e35768e3009853313
Kiki <ismajlikiki@gmail.com>
2024-05-31 14:26:40 -0500

    Completed sticky header functionality.

--- app/assets/scripts/modules/StickyHeader.js
+++ app/assets/scripts/modules/StickyHeader.js
@@ -1,22 +1,63 @@
-import throttle from 'lodash/throttle'
+import throttle from 'lodash/throttle';
+import debounce from 'lodash/debounce';
 
 class StickyHeader {
     constructor() {
-        this.siteHeader = document.querySelector(".site-header")
-        this.events()
+        this.siteHeader = document.querySelector(".site-header");
+        this.pageSections = document.querySelectorAll(".page-section");
+        this.browserHeight = window.innerHeight;
+        this.previousScrollY = window.scrollY;
+        this.scrollDirection = null;
+        this.events();
     }
 
     events() {
-        window.addEventListener("scroll", throttle(() => this.runOnScroll(), 200))
+        window.addEventListener("scroll", throttle(() => this.runOnScroll(), 200));
+        window.addEventListener("resize", debounce(() => {
+            this.browserHeight = window.innerHeight;
+        }, 333));
     }
 
     runOnScroll() {
+        this.determineScrollDirection();
+
         if (window.scrollY > 60) {
-            this.siteHeader.classList.add("site-header--dark")
+            this.siteHeader?.classList.add("site-header--dark");
         } else {
-            this.siteHeader.classList.remove("site-header--dark")
+            this.siteHeader?.classList.remove("site-header--dark");
         }
 
+        this.pageSections.forEach(el => this.calcSection(el));
+    }
+
+    determineScrollDirection() {
+        if (window.scrollY > this.previousScrollY) {
+            this.scrollDirection = "down";
+        } else {
+            this.scrollDirection = "up";
+        }
+        this.previousScrollY = window.scrollY
+    }
+
+    calcSection(el) {
+        if (window.scrollY + this.browserHeight > el.offsetTop && window.scrollY < el.offsetTop + el.offsetHeight) {
+            let scrollPercent = el.getBoundingClientRect().y / this.browserHeight * 100;
+            if ((scrollPercent < 18 && scrollPercent > -0.1 && this.scrollDirection === 'down') ||
+                (scrollPercent < 33 && this.scrollDirection === 'up')) {
+                let matchingLink = el.getAttribute("data-matching-link");
+                if (matchingLink) {
+                    document.querySelectorAll(`.primary-nav a:not(${matchingLink})`).forEach(linkEl => linkEl.classList.remove("is-current-link"));
+                    let matchingLinkEl = document.querySelector(matchingLink);
+                    if (matchingLinkEl) {
+                        matchingLinkEl.classList.add("is-current-link");
+                    } else {
+                        console.error(`No element found for matching link: ${matchingLink}`);
+                    }
+                } else {
+                    console.error('Matching link attribute not found on element:', el);
+                }
+            }
+        }
     }
 }
 

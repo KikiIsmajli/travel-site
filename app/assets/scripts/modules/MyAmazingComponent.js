0b335b1eaa6334ac7fdda33569cbd95951d0061d
Kiki <ismajlikiki@gmail.com>
2024-06-11 19:06:44 -0500

    React added

--- app/assets/scripts/modules/MyAmazingComponent.js
+++ app/assets/scripts/modules/MyAmazingComponent.js
@@ -3,7 +3,7 @@ import React from 'react'
 function MyAmazingComponent() {
     return (
         <div>
-            <h1 className='section-title section-title--blue'>This Is My Amazing React Compomemt</h1>
+            <h1 className='section-title section-title--blue'>This Is My Amazing React Component</h1>
             <p>React is great!!!</p>
         </div>
     )
